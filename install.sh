e99ff6a6ab58e3a797d17137e61955988e9809b9
Adam Archer <agarcher@gmail.com>
2019-05-31 04:11:11 +0000

    automate vim config

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 apt install --assume-yes vim
 apt install --assume-yes curl
-curl -fLo ~/.vim/autoload/plug.vim --create-dir https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -fs $PWD/.vimrc ~/.vimrc
